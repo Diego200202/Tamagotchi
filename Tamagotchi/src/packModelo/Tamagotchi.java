fa8199fe0c9d10cf2384798ad00d648def8016d9
Diego200202 <diegoeugenio61@gmail.com>
2024-03-11 18:48:23 +0100

    metodos basicos cuidados tamagotchi

--- Tamagotchi/src/packModelo/Tamagotchi.java
+++ Tamagotchi/src/packModelo/Tamagotchi.java
@@ -2,6 +2,7 @@ package packModelo;
 
 import java.util.Observable;
 import java.util.Observer;
+import java.util.Random;
 
 import packModelo.evoluciones.Egg;
 import packModelo.evoluciones.Evoluciones;
@@ -12,11 +13,37 @@ public class Tamagotchi extends Observable{
     private int hambre;
     private int hambreMaxima;
     private Evoluciones evo = new Egg();
+    private boolean estaMuerto = false;
+    private boolean estaCagado = false;
+    private boolean estaEnfermo = false;
+    private Random random = new Random();
 
     public Tamagotchi(){
         this.vida = 40;
         this.vidaMaxima = 40;
         this.hambre = 40;
         this.hambreMaxima = 40;  
+    }
+
+    public void ponerEnfermo(){
+        int num = random.nextInt(101);
+        if(num <= 30){
+            this.estaEnfermo = true;
+        }
+    }
+
+    public void cagarse(){
+        int num = random.nextInt(101);
+        if(num <= 20){
+            this.estaCagado = true;
+        }
+    }
+
+    public void limpiar(){
+        this.estaCagado = false;
+    }
+
+    public void curar(){
+        this.estaEnfermo = false;
     }
 }
